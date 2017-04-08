622d5a34bef37ff3908a086a14576a3b965e9213
George-Gate <terrorist_1992@hotmail.com>
2017-04-09 03:19:23 +0800

    Revert "getCoeff"
    This reverts commit e9e69ee0c47e7b050bd964ea6e6755c4bd5213dd.

--- testCode.m
+++
@@ -1,4 +0,0 @@
-mesh=makeMesh('boxSegUniform',{3,2},{[],[]});
-mesh0=mesh;
-K=100;
-[ No2fun, fun2No, Nbasis, hxList, hyList, xList, yList, Svec, Cxvec, Cyvec, Mvec ] = getCoeffs2D_Lobatto( mesh,K );
